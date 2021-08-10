4181e083dae742a1eea84a65c19caad4e437fa3c
Samis6 <30627976+Samis6@users.noreply.github.com>
2021-08-09 21:46:19 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import express, { json, urlencoded } from 'express';
+const express = require('express')
 //const bodyParser = require('body-parser')
-import { request } from 'express';
-import { WebhookClient } from 'dialogflow-fulfillment';
+const { request } = require('express')
+const {WebhookClient} = require('dialogflow-fulfillment');
 
 const app = express()
 //app.use(bodyParser.json())
 const port = process.env.PORT || 3000
-app.use(json())
-app.use(urlencoded({ extended: true}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
 
 app.post('/dialogflow-fullfillment', (request, response)=>{
     dialogflowFullfillment(request, response)
